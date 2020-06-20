7912f77aa8956f6e0a2f38069bd4e8841160dda0
idenilsonsantos <idenilsonoliveira.santos@gmail.com>
2020-06-20 15:41:05 -0300

    feat: add channel data component

--- src/components/ServerName/styles.ts
+++ src/components/ServerName/styles.ts
@@ -4,11 +4,8 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-
     padding: 0 12px 0 16px;
-
     background-color: var(--secondary);
-
     box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
     z-index: 2;
 `;
